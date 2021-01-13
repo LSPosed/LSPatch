7a7757c35ea74d00ed258353e3f1502d4915506c
夏万里 <xiawanli@bytedance.com>
2021-01-13 22:14:17 +0800

    update SandHook to the newest version, to support android11 and fix some hook bugs

--- xpatch/src/main/java/com/storm/wind/xpatch/task/SoAndDexCopyTask.java
+++ xpatch/src/main/java/com/storm/wind/xpatch/task/SoAndDexCopyTask.java
@@ -122,7 +122,7 @@ public class SoAndDexCopyTask implements Runnable {
         if (useWhaleHookFramework) {
             dexAssetPath = "assets/dex/whale/classes-1.0.dex";
         } else {
-            dexAssetPath = "assets/dex/sandhook/classes-1.0.dex";
+            dexAssetPath = "assets/dex/sandhook/classes-2.0.dex";
         }
         FileUtils.copyFileFromJar(dexAssetPath, unzipApkFilePath + copiedDexFileName);
     }
