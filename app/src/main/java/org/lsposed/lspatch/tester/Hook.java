5cd3f73ed1f19e907de3975cab7a6409b8683741
LoveSy <shana@zju.edu.cn>
2021-06-21 20:02:35 +0800

    Fix typo

--- app/src/main/java/org/lsposed/lspatch/tester/Hook.java
+++ app/src/main/java/org/lsposed/lspatch/tester/Hook.java
@@ -8,7 +8,7 @@ import de.robv.android.xposed.callbacks.XC_LoadPackage;
 public class Hook implements IXposedHookLoadPackage {
     @Override
     public void handleLoadPackage(XC_LoadPackage.LoadPackageParam lpparam) throws Throwable {
-        XposedHelpers.findAndHookMethod("org.lsposed.org.lsposed.org.lsposed.lspatch.tester.MainActivity", lpparam.classLoader, "checkXposed2", new XC_MethodHook() {
+        XposedHelpers.findAndHookMethod("org.lsposed.lspatch.tester.MainActivity", lpparam.classLoader, "checkXposed2", new XC_MethodHook() {
             @Override
             protected void afterHookedMethod(MethodHookParam param) throws Throwable {
                 param.setResult(true);
