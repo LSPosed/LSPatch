3d40bdfc0dbdfb0c8c445f4c0289055b4ac236b0
Js0n <29531167+JasonKhew96@users.noreply.github.com>
2022-10-30 15:40:52 +0800

    fix: exclude `androidx.compose.ui.tooling` from release apk

--- manager/build.gradle.kts
+++ manager/build.gradle.kts
@@ -89,14 +89,17 @@ dependencies {
     val roomVersion = "2.4.3"
     val accompanistVersion = "0.26.5-rc"
     val composeDestinationsVersion = "1.7.22-beta"
+    val composeVersion = "1.3.0-rc01"
+
     annotationProcessor("androidx.room:room-compiler:$roomVersion")
     compileOnly("dev.rikka.hidden:stub:2.3.1")
     implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.activity:activity-compose:1.6.0")
     implementation("androidx.compose.material:material-icons-extended:1.3.0-rc01")
     implementation("androidx.compose.material3:material3:1.0.0-rc01")
-    implementation("androidx.compose.ui:ui:1.3.0-rc01")
-    implementation("androidx.compose.ui:ui-tooling:1.3.0-rc01")
+    implementation("androidx.compose.ui:ui:$composeVersion")
+    implementation("androidx.compose.ui:ui-tooling-preview:$composeVersion")
+    debugImplementation("androidx.compose.ui:ui-tooling:$composeVersion")
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.5.1")
     implementation("androidx.navigation:navigation-compose:2.5.2")
     implementation("androidx.preference:preference:1.2.0")
