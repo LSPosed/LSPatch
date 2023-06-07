5d927f1dd27302579a2c9557fe169275204bc8d6
JingMatrix <jingmatrix@gmail.com>
2023-06-07 18:11:47 +0200

    Fix file permissions for Android 14 (#201)
    Dex file should not be writable, otherwise it causes
    java.lang.SecurityException.
    
    Here is the related [soucre
    code](https://cs.android.com/android/platform/superproject/+/master:art/runtime/native/dalvik_system_DexFile.cc;l=381)
    of Android 14.
    
    ---------
    
    Co-authored-by: LoveSy <shana@zju.edu.cn>

--- patch-loader/src/main/java/org/lsposed/lspatch/loader/LSPApplication.java
+++ patch-loader/src/main/java/org/lsposed/lspatch/loader/LSPApplication.java
@@ -137,6 +137,7 @@ public class LSPApplication {
                     Files.copy(is, cacheApkPath);
                 }
             }
+            cacheApkPath.toFile().setWritable(false);
 
             var mPackages = (Map<?, ?>) XposedHelpers.getObjectField(activityThread, "mPackages");
             mPackages.remove(appInfo.packageName);
