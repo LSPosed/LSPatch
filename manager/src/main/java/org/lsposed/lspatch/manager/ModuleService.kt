7201c46c4fbfc680ec513ee1b7fd10e1854cf30c
Js0n <29531167+JasonKhew96@users.noreply.github.com>
2023-11-11 20:43:09 +0800

    fix(manager): keep alive service crashing in A14

--- manager/src/main/java/org/lsposed/lspatch/manager/ModuleService.kt
+++ manager/src/main/java/org/lsposed/lspatch/manager/ModuleService.kt
@@ -4,6 +4,8 @@ import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.app.Service
 import android.content.Intent
+import android.content.pm.ServiceInfo
+import android.os.Build
 import android.os.IBinder
 import android.util.Log
 import androidx.core.app.NotificationCompat
@@ -23,7 +25,19 @@ class ModuleService : Service() {
             val channel = NotificationChannel(Constants.MANAGER_PACKAGE_NAME, TAG, NotificationManager.IMPORTANCE_DEFAULT)
             val manager = getSystemService(NOTIFICATION_SERVICE) as NotificationManager
             manager.createNotificationChannel(channel)
-            startForeground(1, NotificationCompat.Builder(this, Constants.MANAGER_PACKAGE_NAME).build())
+            // TODO: https://developer.android.com/guide/components/bound-services
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.UPSIDE_DOWN_CAKE) {
+                startForeground(
+                    1,
+                    NotificationCompat.Builder(this, Constants.MANAGER_PACKAGE_NAME).build(),
+                    ServiceInfo.FOREGROUND_SERVICE_TYPE_SPECIAL_USE
+                )
+            } else {
+                startForeground(
+                    1,
+                    NotificationCompat.Builder(this, Constants.MANAGER_PACKAGE_NAME).build()
+                )
+            }
         } else {
             stopForeground(STOP_FOREGROUND_REMOVE)
         }
