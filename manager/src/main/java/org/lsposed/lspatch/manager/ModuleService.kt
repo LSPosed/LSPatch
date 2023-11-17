d3cd98694e15cfdc56e9dc58239e787be7952b1b
Js0n <29531167+JasonKhew96@users.noreply.github.com>
2023-11-17 22:36:30 +0800

    Remove keep alive foreground service because bound service won't need it

--- manager/src/main/java/org/lsposed/lspatch/manager/ModuleService.kt
+++ manager/src/main/java/org/lsposed/lspatch/manager/ModuleService.kt
@@ -1,47 +1,15 @@
 package org.lsposed.lspatch.manager
 
-import android.app.NotificationChannel
-import android.app.NotificationManager
 import android.app.Service
 import android.content.Intent
-import android.content.pm.ServiceInfo
-import android.os.Build
 import android.os.IBinder
 import android.util.Log
-import androidx.core.app.NotificationCompat
-import org.lsposed.lspatch.config.Configs
-import org.lsposed.lspatch.share.Constants
 
 
 class ModuleService : Service() {
 
     companion object {
         private const val TAG = "ModuleService"
-    }
-
-    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
-        intent ?: return START_NOT_STICKY
-        if (Configs.keepAlive == Configs.KeepAlive.FOREGROUND) {
-            val channel = NotificationChannel(Constants.MANAGER_PACKAGE_NAME, TAG, NotificationManager.IMPORTANCE_DEFAULT)
-            val manager = getSystemService(NOTIFICATION_SERVICE) as NotificationManager
-            manager.createNotificationChannel(channel)
-            // TODO: https://developer.android.com/guide/components/bound-services
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.UPSIDE_DOWN_CAKE) {
-                startForeground(
-                    1,
-                    NotificationCompat.Builder(this, Constants.MANAGER_PACKAGE_NAME).build(),
-                    ServiceInfo.FOREGROUND_SERVICE_TYPE_SPECIAL_USE
-                )
-            } else {
-                startForeground(
-                    1,
-                    NotificationCompat.Builder(this, Constants.MANAGER_PACKAGE_NAME).build()
-                )
-            }
-        } else {
-            stopForeground(STOP_FOREGROUND_REMOVE)
-        }
-        return super.onStartCommand(intent, flags, startId)
     }
 
     override fun onBind(intent: Intent): IBinder? {
