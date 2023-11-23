3674a967ef3b7335998d668f8b057b47269e9aff
Js0n <29531167+JasonKhew96@users.noreply.github.com>
2023-11-23 20:32:24 +0800

    Remove keep alive foreground service because bound service won't need it (#262)
    Co-authored-by: LoveSy <shana@zju.edu.cn>

--- manager/src/main/java/org/lsposed/lspatch/config/Configs.kt
+++ manager/src/main/java/org/lsposed/lspatch/config/Configs.kt
@@ -1,8 +1,6 @@
 package org.lsposed.lspatch.config
 
-import android.content.Intent
 import org.lsposed.lspatch.lspApp
-import org.lsposed.lspatch.manager.ModuleService
 import org.lsposed.lspatch.ui.util.delegateStateOf
 import org.lsposed.lspatch.ui.util.getValue
 import org.lsposed.lspatch.ui.util.setValue
@@ -14,11 +12,6 @@ object Configs {
     private const val PREFS_KEYSTORE_ALIAS_PASSWORD = "keystore_alias_password"
     private const val PREFS_STORAGE_DIRECTORY = "storage_directory"
     private const val PREFS_DETAIL_PATCH_LOGS = "detail_patch_logs"
-    private const val PREFS_KEEP_ALIVE = "keep_alive"
-
-    enum class KeepAlive {
-        OFF, FOREGROUND
-    }
 
     var keyStorePassword by delegateStateOf(lspApp.prefs.getString(PREFS_KEYSTORE_PASSWORD, "123456")!!) {
         lspApp.prefs.edit().putString(PREFS_KEYSTORE_PASSWORD, it).apply()
@@ -38,10 +31,5 @@ object Configs {
 
     var detailPatchLogs by delegateStateOf(lspApp.prefs.getBoolean(PREFS_DETAIL_PATCH_LOGS, true)) {
         lspApp.prefs.edit().putBoolean(PREFS_DETAIL_PATCH_LOGS, it).apply()
-    }
-
-    var keepAlive by delegateStateOf(KeepAlive.values()[lspApp.prefs.getInt(PREFS_KEEP_ALIVE, KeepAlive.OFF.ordinal)]) {
-        lspApp.prefs.edit().putInt(PREFS_KEEP_ALIVE, it.ordinal).apply()
-        lspApp.startService(Intent(lspApp, ModuleService::class.java))
     }
 }
--- manager/src/main/java/org/lsposed/lspatch/ui/page/SettingsScreen.kt
+++ manager/src/main/java/org/lsposed/lspatch/ui/page/SettingsScreen.kt
@@ -13,7 +13,6 @@ import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.outlined.Ballot
 import androidx.compose.material.icons.outlined.BugReport
-import androidx.compose.material.icons.outlined.HourglassEmpty
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.runtime.saveable.rememberSaveable
@@ -51,7 +50,6 @@ fun SettingsScreen() {
         ) {
             KeyStore()
             DetailPatchLogs()
-            KeepAlive()
         }
     }
 }
@@ -241,40 +239,4 @@ private fun DetailPatchLogs() {
         icon = Icons.Outlined.BugReport,
         title = stringResource(R.string.settings_detail_patch_logs)
     )
-}
-
-@Composable
-private fun KeepAlive() {
-    var expanded by remember { mutableStateOf(false) }
-    AnywhereDropdown(
-        expanded = expanded,
-        onDismissRequest = { expanded = false },
-        onClick = { expanded = true },
-        surface = {
-            val desc = when (Configs.keepAlive) {
-                Configs.KeepAlive.OFF -> R.string.off
-                Configs.KeepAlive.FOREGROUND -> R.string.settings_keep_alive_foreground
-            }
-            SettingsItem(
-                icon = Icons.Outlined.HourglassEmpty,
-                title = stringResource(R.string.settings_keep_alive),
-                desc = stringResource(desc)
-            )
-        }
-    ) {
-        DropdownMenuItem(
-            text = { Text(stringResource(R.string.off)) },
-            onClick = {
-                Configs.keepAlive = Configs.KeepAlive.OFF
-                expanded = false
-            }
-        )
-        DropdownMenuItem(
-            text = { Text(stringResource(R.string.settings_keep_alive_foreground)) },
-            onClick = {
-                Configs.keepAlive = Configs.KeepAlive.FOREGROUND
-                expanded = false
-            }
-        )
-    }
 }
